fef387a0f3dfe9b90490001740e8cffa84a58098
HusYada <h_geyik@outlook.com>
2024-09-04 04:12:21 +0100

    double / shadow collision is in!
    array loop check for valid index next

--- src/main.cpp
+++ src/main.cpp
@@ -26,56 +26,68 @@ void test_scene(bn::camera_ptr& camera, bn::sprite_text_generator text_generator
 	bn::regular_bg_ptr background_map = bn::regular_bg_items::test_map_3.create_bg(0,0);
 	
 	bn::sprite_ptr plyr_pixel = bn::sprite_items::cursor.create_sprite(0, 0);
-	bn::sprite_ptr plyr_current_tile = bn::sprite_items::cursor.create_sprite(0, 0);
-	plyr_current_tile.set_tiles(bn::sprite_items::cursor.tiles_item().create_tiles(2));
+	bn::sprite_ptr plyr_left_shadow = bn::sprite_items::cursor.create_sprite(0, 0);
+    bn::sprite_ptr plyr_rght_shadow = bn::sprite_items::cursor.create_sprite(0, 0);
+	plyr_left_shadow.set_tiles(bn::sprite_items::cursor.tiles_item().create_tiles(2));
+    plyr_rght_shadow.set_tiles(bn::sprite_items::cursor.tiles_item().create_tiles(2));
 
 	const bn::regular_bg_map_item& map_item = bn::regular_bg_items::test_map_3.map_item();
 	bn::regular_bg_map_cell valid_map_cell = map_item.cell(0, 0);
 	int valid_tile_index = bn::regular_bg_map_cell_info(valid_map_cell).tile_index();
 
-    bn::point plyr_map_position(255, 255);
+    bn::point leftshadowpos(255, 255);
+    bn::point rghtshadowpos(256, 255);
     background_map.set_camera(camera);
-    plyr_current_tile.set_camera(camera);
+    plyr_left_shadow.set_camera(camera);
+    plyr_rght_shadow.set_camera(camera);
 
     // ----------------------------------------------------
 	while(true)
     {
-    	bn::point new_plyr_map_position = plyr_map_position;
+    	bn::point newleftshadowpos = leftshadowpos;
+        bn::point newrghtshadowpos = rghtshadowpos;
 
         if(bn::keypad::left_pressed())
         {
-            new_plyr_map_position.set_x(new_plyr_map_position.x() - 1);
+            newleftshadowpos.set_x(newleftshadowpos.x() - 1);
+            newrghtshadowpos.set_x(newrghtshadowpos.x() - 1);
             scrollx -= 1;
         }
         else if(bn::keypad::right_pressed())
         {
-            new_plyr_map_position.set_x(new_plyr_map_position.x() + 1);
+            newleftshadowpos.set_x(newleftshadowpos.x() + 1);
+            newrghtshadowpos.set_x(newrghtshadowpos.x() + 1);
             scrollx += 1;
         }
         if(bn::keypad::up_pressed())
         {
-            new_plyr_map_position.set_y(new_plyr_map_position.y() - 1);
+            newleftshadowpos.set_y(newleftshadowpos.y() - 1);
+            newrghtshadowpos.set_y(newrghtshadowpos.y() - 1);
             scrolly -= 1;
         }
         else if(bn::keypad::down_pressed())
         {
-            new_plyr_map_position.set_y(new_plyr_map_position.y() + 1);
+            newleftshadowpos.set_y(newleftshadowpos.y() + 1);
+            newrghtshadowpos.set_y(newrghtshadowpos.y() + 1);
             scrolly += 1;
         }
 
-        bn::regular_bg_map_cell plyr_map_cell = map_item.cell(new_plyr_map_position);
-        int plyr_tile_index = bn::regular_bg_map_cell_info(plyr_map_cell).tile_index();
+        bn::regular_bg_map_cell plyr_map_cell1 = map_item.cell(newleftshadowpos);
+        bn::regular_bg_map_cell plyr_map_cell2 = map_item.cell(newrghtshadowpos);
+        int plyr_tile_index1 = bn::regular_bg_map_cell_info(plyr_map_cell1).tile_index();
+        int plyr_tile_index2 = bn::regular_bg_map_cell_info(plyr_map_cell2).tile_index();
 
-        bn::fixed plyr_sprite_x = (plyr_map_position.x() * 8) - (map_item.dimensions().width() * 4) + 4;
-        bn::fixed plyr_sprite_y = (plyr_map_position.y() * 8) - (map_item.dimensions().height() * 4) + 4;
-        plyr_current_tile.set_position(plyr_sprite_x, plyr_sprite_y);
-        //plyr_current_tile.set_position(4, 4);
+        bn::fixed plyr_sprite_x = (leftshadowpos.x() * 8) - (map_item.dimensions().width() * 4) + 4;
+        bn::fixed plyr_sprite_y = (leftshadowpos.y() * 8) - (map_item.dimensions().height() * 4) + 4;
+        plyr_left_shadow.set_position(plyr_sprite_x, plyr_sprite_y);
+        plyr_rght_shadow.set_position(plyr_sprite_x + 8, plyr_sprite_y);
 
-        if(plyr_tile_index == valid_tile_index)
+        if(plyr_tile_index1 == valid_tile_index && plyr_tile_index2 == valid_tile_index)
         {
         	camera.set_x(plyr_sprite_x + 4 + 0);
-        	camera.set_y(plyr_sprite_y + 4 + 0);
-            plyr_map_position = new_plyr_map_position;
+        	camera.set_y(plyr_sprite_y + 4 - 24);
+            leftshadowpos = newleftshadowpos;
+            rghtshadowpos = newrghtshadowpos;
         }
 
         // ----------------------------------------------------
